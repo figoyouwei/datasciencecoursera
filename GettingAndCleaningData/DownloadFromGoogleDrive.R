d712525dc66c460776b196da495813077e2a4142
Youwei Zheng <figoyouwei@gmail.com>
2014-07-08 11:18:53 -0500

    R downloading from Dropbox works

--- GettingAndCleaningData/DownloadFromGoogleDrive.R
+++ GettingAndCleaningData/DownloadFromGoogleDrive.R
@@ -1,14 +1,21 @@
-# ----- checking existing or creating new directories 
-if (!file.exists("./Data")) {
-    dir.create("./Data")
+dl_from_GoogleD <- function(key, localpath) {    
+    ## Arguments:
+    ## output = output file name
+    ## key = Google document key
+    ## format = output format (pdf, rtf, doc, txt..)
+    ## Note: File must be shareable!
+    
+    require(RCurl)
+    #cyberpath <- paste0("https://docs.google.com/file/d/", key, "/export?format=", format)
+    cyberpath <- paste0("https://docs.google.com/file/d/", key)
+    bin <- getBinaryURL(cyberpath, ssl.verifypeer = FALSE)
+    con <- file(localpath, open = "wb")
+    writeBin(bin, con)
+    close(con)
+    message(noquote(paste(localpath, "read into", getwd())))
 }
-#unlink("./Data", recursive = TRUE) #remove directory
 
-# ----- downloading data from the internet 
-fileURL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?
-            accessType=DOWNLOAD"
-download.file(fileURL,destfile="./Data/cameras.csv",method="curl")
-
-# ----- list all files of a directory and date it 
-list.files("./Data")
-dateDownloaded <- date()
+# Example:
+dl_from_GoogleD(key = "0B937jFpi7V9RN3QwOE9ZOWt3Q2c",
+                localpath = "./Data/cameras.csv")
+cameraData <- read.table("./Data/cameras.csv",sep=",",header=TRUE)
--- GettingAndCleaningData/DownloadingFiles.R
+++ GettingAndCleaningData/DownloadingFiles.R
@@ -1,18 +1,28 @@
 # ----- checking existing or creating new directories 
+# unlink("./Data", recursive = TRUE) #remove directory
+rm(list=ls())
+cat("\014")
 if (!file.exists("./Data")) {
     dir.create("./Data")
 }
-#unlink("./Data", recursive = TRUE) #remove directory
 
-# ----- downloading data from the internet 
-fileURL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?
+# ----- downloading pdf from Open-Access Journal 
+fileURL <- "http://www.plosone.org/article/fetchObject.action?uri=info%3Adoi%2F10.1371%2Fjournal.pone.0101592&representation=PDF"
+download.file(fileURL,destfile="./one.pdf",method="curl")
+
+# ----- downloading csv from some data website
+fileURL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?    
             accessType=DOWNLOAD"
 download.file(fileURL,destfile="./Data/cameras.csv",method="curl")
 
+# ----- downloading shared pdf from Cloud 
+fileURL <- "https://www.dropbox.com/s/2qsu73p99bqp6bn/23%20Problems%20in%20Computational%20Neuroscience.pdf?dl=1"
+download.file(fileURL,destfile="./one.pdf",method="curl")
+
 # ----- list all files of a directory and date it 
-list.files("./Data")
+listpath <- "./"
+list.files(listpath)
 dateDownloaded <- date()
 
-# ----- read local flat data
+# ----- read local flat data (read.csv auto sets sep="," and header=TRUE)
 cameraData <- read.table("./Data/cameras.csv",sep=",",header=TRUE)
-#read.csv auto sets sep="," and header=TRUE
