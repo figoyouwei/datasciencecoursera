358913b3bd6a44a27a66ba7036f066639715871b
Youwei Zheng <figoyouwei@gmail.com>
2014-07-12 11:50:29 -0500

    GettingAndCleaningData Quiz 1 Finish

--- GettingAndCleaningData/DownloadingFiles.R
+++ GettingAndCleaningData/DownloadingFiles.R
@@ -6,17 +6,22 @@ if (!file.exists("./Data")) {
     dir.create("./Data")
 }
 
-# ----- downloading pdf from Open-Access Journal 
-fileURL <- "http://www.plosone.org/article/fetchObject.action?uri=info%3Adoi%2F10.1371%2Fjournal.pone.0101592&representation=PDF"
+# ----- downloading csv from some data website
+fileURL_csv <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?    
+            accessType=DOWNLOAD"
+download.file(fileURL_csv,destfile="./Data/cameras.csv",method="curl")
+
+# ----- downloading xlsx from some data website
+fileURL_xlsx <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.xlsx?    
+                accessType=DOWNLOAD"
+download.file(fileURL_xlsx,destfile="./Data/cameras.xlsx",method="curl")
+
+# ----- downloading shared pdf from Cloud, works!
+fileURL <- "https://www.dropbox.com/s/2qsu73p99bqp6bn/23%20Problems%20in%20Computational%20Neuroscience.pdf?dl=1"
 download.file(fileURL,destfile="./one.pdf",method="curl")
 
-# ----- downloading csv from some data website
-fileURL <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?    
-            accessType=DOWNLOAD"
-download.file(fileURL,destfile="./Data/cameras.csv",method="curl")
-
-# ----- downloading shared pdf from Cloud 
-fileURL <- "https://www.dropbox.com/s/2qsu73p99bqp6bn/23%20Problems%20in%20Computational%20Neuroscience.pdf?dl=1"
+# ----- downloading pdf from Open-Access Journal,works!
+fileURL <- "http://www.plosone.org/article/fetchObject.action?uri=info%3Adoi%2F10.1371%2Fjournal.pone.0101592&representation=PDF"
 download.file(fileURL,destfile="./one.pdf",method="curl")
 
 # ----- list all files of a directory and date it 
@@ -26,3 +31,37 @@ dateDownloaded <- date()
 
 # ----- read local flat data (read.csv auto sets sep="," and header=TRUE)
 cameraData <- read.table("./Data/cameras.csv",sep=",",header=TRUE)
+
+# ----- read Excel data 
+library(xlsx)
+rows <- 1:5 # row 1 is the header.
+cols <- 1:2
+cameraDataSub <- read.xlsx("./Data/cameras.xlsx",sheetIndex=1,
+                        rowIndex=rows,colIndex=cols)
+
+# ----- read XML directly from the web, a food-menu example 
+library(XML)
+fileURL_XML <- "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(fileURL_XML,useInternal=TRUE)
+content <- xmlRoot(doc)
+xmlName(content)
+content[[1]][[1]]
+xpathSApply(content,"//name",xmlValue)
+xpathSApply(content,"//price",xmlValue)
+
+# ----- read html directly from the web, a sport-team page ...
+fileURL_HTML <- "http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+doc <- htmlTreeParse(fileURL_HTML,useInternal=TRUE)
+teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
+
+# ----- read JSON directly from the web, a github api
+library(jsonlite)
+jsonData <- fromJSON("https://api.github.com/users/figoyouwei/repos")
+names(jsonData)
+
+# ----- write R data.frame to JSON
+library(datasets)
+data(iris)
+myjson <- toJSON(iris,pretty=TRUE)
+cat(myjson)
+iris2 <- fromJSON(myjson)
---
+++ GettingAndCleaningData/Quiz1/answers.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+cat("\014")
+
+# ----- Q1
+fileURL_Q1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileURL_Q1,destfile="./Data/Quiz1_Q1.csv",method="curl")
+fileURL_pdf <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf"
+download.file(fileURL_pdf,destfile="./Data/Quiz1_Q1.pdf",method="curl")
+data_Q1 <- read.table("./Data/Quiz1_Q1.csv",sep=",",header=TRUE)
+# found variable name from cookbook, and calculate 
+indices <- data_Quiz_Q1$VAL >= 24
+sum(indices, na.rm=TRUE)
+
+# ----- Q3
+fileURL_Q3 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileURL_Q3,destfile="./Data/Quiz1_Q3.xlsx",method="curl")
+library(xlsx)
+rows <- 18:23 
+cols <- 7:15
+data_Q3 <- read.xlsx("./Data/Quiz1_Q3.xlsx",sheetIndex=1,
+                     rowIndex=rows,colIndex=cols,header=TRUE)
+sum(data_Q3$Zip*data_Q3$Ext,na.rm=TRUE) 
+
+# ----- Q4: Baltimore Restaurants
+fileURL_Q4 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+download.file(fileURL_Q4,destfile="./Data/Quiz1_Q4.xml",method="curl")
+library(XML)
+doc <- xmlTreeParse("./Data/Quiz1_Q4.xml",useInternal=TRUE)
+content <- xmlRoot(doc)
+zipcodes <- xpathSApply(content,"//zipcode",xmlValue)
+sum(zipcodes == 21231)
+
+# ----- Q5:
+library(data.table)
+fileURL_Q5 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileURL_Q5,destfile="./Data/Quiz1_Q5.csv",method="curl")
+DT <- fread("./Data/Quiz1_Q5.csv")
+
+# data.table is faster apply ...
+ptm <- proc.time()
+DT[,mean(pwgtp15),by=SEX]
+proc.time() - ptm
+
+# this method is nice ... team-up apply to each team. 
+# apply calculation (mean) on pwgtp15, based on SEX. 
+ptm <- proc.time()
+tapply(DT$pwgtp15,DT$SEX,mean)
+proc.time() - ptm
+
+ptm <- proc.time()
+sapply(split(DT$pwgtp15,DT$SEX),mean)
+proc.time() - ptm
