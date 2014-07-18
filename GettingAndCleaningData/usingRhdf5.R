ed9519e2b7274cc3358d10c0ffe9ad6d8c062f96
Youwei Zheng <figoyouwei@gmail.com>
2014-07-18 10:26:13 -0500

    went through rhdf5 material from Bernd Fischer

--- GettingAndCleaningData/usingRhdf5.R
+++ GettingAndCleaningData/usingRhdf5.R
@@ -53,6 +53,55 @@ h5read(filename,"foo/S")
 h5read(filename,"foo/S",index=list(3:4,1:2))
 h5read(filename,"foo/S",index=list(3:4,c(2,4,6)))
 
-# write subset with hyperlabs: "start,stride,count,block" (page 5)
+# ----- write subset with hyperlabs: "start,stride,count,block" (page 5)
+
+# use only the "start"
+h5createDataset(filename,"foo/H",c(5,8),storage.mode="integer",chunk=c(5,1),level=7)
+h5read(filename,"foo/H")
+h5write(matrix(1:5,nr=5,nc=1),file=filename,name="foo/H",start=c(1,4))
+h5read(filename,"foo/H")
+
+# use "start" and "count"
+h5write(matrix(6:10,nr=5,nc=1),file=filename,name="foo/H",start=c(1,2),count=c(1,5))
+h5read(filename,"foo/H")
+
+# write a submatrix 5*6
+h5write(matrix(11:40,nr=5,nc=6),file=filename,name="foo/H",start=c(1,3))
+h5read(filename,"foo/H")
+
+# write a submatrix 2*2
+h5write(matrix(120:123,nr=2,nc=2),file=filename,name="foo/H",start=c(4,1))
+h5read(filename,"foo/H")
+
+# use "stride", split the assigned matrix 
+h5write(matrix(151:154,nr=2,nc=2),file=filename,name="foo/H",start=c(2,3),
+        stride=c(3,2))
+h5read(filename,"foo/H")
+
+# count while reading 
+h5read(filename,"foo/H",start=c(2,2),count=c(2,2))
+
+# count & stride while reading 
+h5read(filename,"foo/H",start=c(3,3),count=c(2,3))
+h5read(filename,"foo/H",start=c(3,3),count=c(2,3), stride=c(1,2))
+
+# block ???
+h5read(filename,"foo/H",start=c(3,3),count=c(2,3), stride=c(1,2),block=c(1,2))
 
 # ----- 3.4 Saving multiple objects to an HDF5 file (h5save)
+# save to the top level group directly using h5save not h5write !
+h5dump(filename)
+h5ls(filename)
+W <- 1:7
+Y <- 1:18
+Z <- seq(0,1,0.1)
+h5save(W,Y,Z,file=filename)
+
+# ----- 3.5 list the content of an HDF5 file
+h5ls(filename,all=TRUE)
+
+# ----- 3.6 dump the content of an HDF5 file
+D <- h5dump(filename)
+
+# ----- 6 Session info
+toLatex(sessionInfo())
