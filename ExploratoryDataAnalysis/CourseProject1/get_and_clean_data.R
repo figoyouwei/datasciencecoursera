8358dafd0854b31036d6a74772d86b4d177e885c
Youwei Zheng <figoyouwei@gmail.com>
2014-08-09 21:15:46 -0500

    chron package

--- ExploratoryDataAnalysis/CourseProject1/get_and_clean_data.R
+++ ExploratoryDataAnalysis/CourseProject1/get_and_clean_data.R
@@ -8,7 +8,10 @@ dat <- read.table(datpath,sep=";",header=TRUE,colClasses = "character")
 
 # ----- * extract the subset from only the dates 2007-02-01 and 2007-02-02
 # ----- * convert string to date and time format
-dat$DateTime <- as.POSIXct(paste(dat$Date, dat$Time), format="%d/%m/%Y %H:%M:%S")
-dat$Date <- as.Date(dat$Date,format='%d/%m/%Y')
-datplot <- dat[(dat$Date == as.Date("2007-02-01") | 
-                dat$Date == as.Date("2007-02-02")),]
+# dat$dates <- as.POSIXct(paste(dat$Date, dat$Time), format="%d/%m/%Y %H:%M:%S")
+library(chron) # use times function
+dat$dates <- as.Date(dat$Date,format='%d/%m/%Y')
+dat$times <- chron(times=dat$Time)
+
+datplot <- dat[(dat$dates == as.Date("2007-02-01") | 
+                dat$dates == as.Date("2007-02-02")),]
